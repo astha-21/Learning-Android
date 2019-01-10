296dd3976f42b299ffdd6d983d9e956a442fed52
astha-21 <astha6145@gmail.com>
2019-01-10 12:21:40 +0530

    Commit from Android Studio validation of data done

--- app/src/main/java/com/example/asthasharma/learningandroid/MainActivity.java
+++ app/src/main/java/com/example/asthasharma/learningandroid/MainActivity.java
@@ -5,16 +5,60 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import android.view.View.OnClickListener;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passEditText;
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        emailEditText = findViewById(R.id.editText1);
+        passEditText = findViewById(R.id.editText);
+
+        findViewById(R.id.button).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                final String email = emailEditText.getText().toString();
+                if (!isValidEmail(email)) {
+                    emailEditText.setError("Invalid Email");
+                }
+
+                final String pass = passEditText.getText().toString();
+                if (!isValidPassword(pass)) {
+                    passEditText.setError("Invalid Password");
+                }
+
+            }
+        });
     }
+
+    // validating email id
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // validating password with retype password
+    private boolean isValidPassword(String pass) {
+        if ((pass != null) && (pass.length() > 6))
+        {
+            return true;
+        }
+        return false;
+    }
+
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
